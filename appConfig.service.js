53565a38813410bed00c0372dc391bfafa057d32
Kana Kihara <Kana@Kanas-MacBook-Air.local>
2017-06-20 13:58:49 -0400

    added appConfig.service.js

---
+++ appConfig.service.js
@@ -0,0 +1,10 @@
+/*(function(){
+	var app = angular.module("TodoApp"); 
+
+	app.service("AppDataServiceSvc", function(TaskNameSvc){
+		this.name = AppNameSvc,
+		this.author = "Koushik",
+		this.builtDate = new Date().toDateString();
+
+	});
+})();*/
